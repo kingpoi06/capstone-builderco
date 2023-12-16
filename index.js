99438e49dadf78cf7b0405d70965b3c12d22b1c8
kingpoi06 <putratyo90@gmail.com>
2023-12-17 01:57:27 +0700

    update commit

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ require('dotenv').config();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Use CORS middleware
+// Gunakan middleware CORS
 app.use(cors());
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+// Sajikan file statis dari direktori "public" dengan menetapkan Content-Type
+app.use('/public', (req, res, next) => {
+  const filePath = req.path;
+  
+  if (filePath.endsWith('.js')) {
+    res.header('Content-Type', 'application/javascript');
+  }
+
+  express.static('public')(req, res, next);
+});
 
 const tukangRouter = require('./routes/tukang-router');
 const authRouter = require('./routes/auth-router');
@@ -23,5 +31,5 @@ app.use("/api/v1/auth", authRouter);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("Server is running....");
+    console.log("Server sedang berjalan....");
 });
