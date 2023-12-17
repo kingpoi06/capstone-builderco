100e0026f74840cd3c0353bca387e05d86f2c183
kingpoi06 <putratyo90@gmail.com>
2023-12-17 16:28:57 +0700

    update commit

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 const tukangRouter = require('./routes/tukang-router');
 const authRouter = require('./routes/auth-router');
 
