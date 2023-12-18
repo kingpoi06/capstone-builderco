922233275673b6e573c0efdaded6182f0168a105
kingpoi06 <putratyo90@gmail.com>
2023-12-18 16:39:38 +0700

    update commit

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+  const allowedOrigins = ['http://localhost:5000', 'https://your-netlify-app.netlify.app'];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 const tukangRouter = require('./routes/tukang-router');
 const authRouter = require('./routes/auth-router');
@@ -30,3 +37,25 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("Server is running....");
 });
+
+// app.use((req, res, next) => {
+//   const allowedOrigins = ['http://localhost:5000', 'https://your-netlify-app.netlify.app'];
+
+//   const origin = req.headers.origin;
+
+//   if (allowedOrigins.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+//   next();
+// });
